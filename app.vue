6ed399c5a18e75e45b29e9d560b86ef723fff2ae
Keidson Roby <keidsondesigner@gmail.com>
2024-11-14 04:38:01 -0400

    Atualiza app.vue para incluir o novo NavMenu e melhorar a estrutura da aplicação

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavMenu />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useCartStore } from '~/stores/cartStore'
+
+const cartStore = useCartStore()
+
+onMounted(() => {
+  cartStore.loadFromLocalStorage()
+})
+</script>
